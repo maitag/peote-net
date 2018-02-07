4227c2fc84a4951ecff7cfa3e134850ab1a596c2
Sylvio Sell <info@maitag.de>
2018-02-07 17:38:01 +0100

    fix buildscript

--- testing/peote-net-test/cli_build.bat
+++ testing/peote-net-test/cli_build.bat
@@ -1,28 +1,34 @@
 haxe cli.hxml
 
+@echo off
 if not exist bin mkdir bin
+@echo on
 
 copy build\neko\peote-net-test.n bin\peote-net-test.n
+
 @echo off
-REM creates (clickable;) batch files to start servers and clients
-(
+REM creates (clickable;) batch files for neko server and client
+if not exist bin\neko_server.bat (
   echo neko peote-net-test.n -s 1
   echo pause
 ) > bin\neko_server.bat
-(
+
+if not exist bin\neko_client.bat (
   echo neko peote-net-test.n -c 1
   echo pause
 ) > bin\neko_client.bat
 @echo on
 
 copy build\cpp\MainCli.exe bin\peote-net-test.exe
+
 @echo off
-REM creates (clickable;) batch files to start servers and clients
-(
+REM creates (clickable;) batch files for cpp server and client
+if not exist bin\server.bat (
   echo peote-net-test.exe -s 1
   echo pause
 ) > bin\server.bat
-(
+
+if not exist bin\client.bat (
   echo peote-net-test.exe -c 1
   echo pause
 ) > bin\client.bat
