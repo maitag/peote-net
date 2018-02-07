e0bc0b61c473015ed4e91f19a12400fc58a183f2
Sylvio Sell <info@maitag.de>
2018-02-07 02:55:21 +0100

    fix peote-net-test linux build script

--- testing/peote-net-test/cli_build.sh
+++ testing/peote-net-test/cli_build.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+
 haxe cli.hxml
 
 test -d "bin" || mkdir -p "bin"
 
 cp build/neko/peote-net-test.n bin/peote-net-test.n
+
+echo '#!/bin/bash\nneko peote-net-test.n "$@"' >bin/peote-net-test-neko
+chmod +x bin/peote-net-test-neko
 
 cp build/cpp/MainCli bin/peote-net-test
